33c72b1d2ba4c8683a19b51c4ce4659163833d92
TheBookwyrms <60119564+TheBookwyrms@users.noreply.github.com>
2025-12-24 22:57:32 -0500

    hid old?????

--- old/general_math_tests/test.py
+++
@@ -1,4 +0,0 @@
-import basic_math_and_functions as b
-
-pi = b.pi_digits(4)
-print(pi.pi)
--- old/sequence_series/__main__.py
+++
@@ -1,8 +0,0 @@
-from math_stuff.node_class import run
-from math_stuff.derive_tree import run_2
-from math_stuff.sequence_series import run_3
-
-if __name__ == "__main__":
-    #run()
-    #run_2()
-    run_3()
